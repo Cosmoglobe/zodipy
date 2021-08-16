69ffed52bae384a461a0275f365e21e6fc3a757c
Metin San <metinisan@gmail.com>
2021-08-16 14:59:25 +0200

    Removed redundant models

--- zodipy/models.py
+++ zodipy/models.py
@@ -9,7 +9,6 @@ PLANCK_2013 = InterplanetaryDustModel(
     emissivities=emissivities.PLANCK_2013
 )
 
-
 PLANCK_2015 = InterplanetaryDustModel(
     components=('cloud', 'band1', 'band2', 'band3'),
     parameters=parameters.K98,
@@ -20,54 +19,4 @@ PLANCK_2018 = InterplanetaryDustModel(
     components=('cloud', 'band1', 'band2', 'band3'),
     parameters=parameters.K98,
     emissivities=emissivities.PLANCK_2018
-)
-
-# Single component models used for testing
-# ----------------------------------------
-_CLOUD = InterplanetaryDustModel(
-    components=('cloud',),
-    parameters=parameters.K98,
-    emissivities=emissivities.PLANCK_2018
-)
-
-_BAND1 = InterplanetaryDustModel(
-    components=('band1',),
-    parameters=parameters.K98,
-    emissivities=emissivities.PLANCK_2018
-)
-
-_BAND2 = InterplanetaryDustModel(
-    components=('band2',),
-    parameters=parameters.K98,
-    emissivities=emissivities.PLANCK_2018
-)
-
-_BAND3 = InterplanetaryDustModel(
-    components=('band3',),
-    parameters=parameters.K98,
-    emissivities=emissivities.PLANCK_2018
-)
-
-_BANDS = InterplanetaryDustModel(
-    components=('band3',),
-    parameters=parameters.K98,
-    emissivities=emissivities.PLANCK_2018
-)
-
-_RING = InterplanetaryDustModel(
-    components=('ring',),
-    parameters=parameters.K98,
-    emissivities=emissivities.PLANCK_2013
-)
-
-_FEATURE = InterplanetaryDustModel(
-    components=('feature',),
-    parameters=parameters.K98,
-    emissivities=emissivities.PLANCK_2013
-)
-
-_CIRCUMSOLAR = InterplanetaryDustModel(
-    components=('ring','feature',),
-    parameters=parameters.K98,
-    emissivities=emissivities.PLANCK_2013
 )
