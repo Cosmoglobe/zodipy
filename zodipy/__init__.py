1802c13c02247f471565dfa34035926bd322c23f
Metin San <metinisan@gmail.com>
2021-11-25 08:36:37 +0100

    Removed custom model registry.

--- zodipy/__init__.py
+++ zodipy/__init__.py
@@ -1,13 +1,3 @@
 from zodipy.core import InterplanetaryDustModel
-from zodipy._model import register_custom_model
-from zodipy.models import model_registry
-import zodipy.parameters as parameters
-import zodipy.emissivities as emissivities
 
-__all__ = (
-    "InterplanetaryDustModel",
-    "register_custom_model",
-    "model_registry",
-    "parameters",
-    "emissivities",
-)
+__all__ = ("InterplanetaryDustModel",)
