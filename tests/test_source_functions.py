19dd2a20be300aa72ccb5b210317e40e7e4aa4ac
Metin San <metinisan@gmail.com>
2022-03-29 09:58:58 +0200

    Updated broken unit tests

--- tests/test_source_functions.py
+++ tests/test_source_functions.py
@@ -16,14 +16,14 @@ FREQUENCY = 549
 def test_blackbody_emission_value():
     """Tests that return value."""
 
-    emission = blackbody_emission_nu(TEMPERATURE, FREQUENCY)
+    emission = blackbody_emission_nu(T=TEMPERATURE, freq=FREQUENCY)
     assert emission == pytest.approx(1.73442848898e-15, abs=1e-20)
 
 
 def test_blackbody_emission_value_array():
     """Tests the return value given a temperature array."""
 
-    emission = blackbody_emission_nu(TEMPERATURE_ARRAY, FREQUENCY)
+    emission = blackbody_emission_nu(T=TEMPERATURE_ARRAY, freq=FREQUENCY)
     true_values = np.array([1.82147825366e-15, 3.06550295038e-15, 3.78860400626e-15])
     assert emission == pytest.approx(true_values, abs=1e-20)
 
@@ -31,14 +31,14 @@ def test_blackbody_emission_value_array():
 def test_blackbody_emission_returns_float():
     """Tests that the returned value is a float given a float temperature."""
 
-    emission = blackbody_emission_nu(TEMPERATURE, FREQUENCY)
+    emission = blackbody_emission_nu(T=TEMPERATURE, freq=FREQUENCY)
     assert isinstance(emission, float)
 
 
 def test_blackbody_emission_returns_array():
     """Tests that the returned value is an array given an array temperature."""
 
-    emission = blackbody_emission_nu(TEMPERATURE_ARRAY, FREQUENCY)
+    emission = blackbody_emission_nu(T=TEMPERATURE_ARRAY, freq=FREQUENCY)
     assert isinstance(emission, np.ndarray)
 
 
