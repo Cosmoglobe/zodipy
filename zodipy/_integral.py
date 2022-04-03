303d6e8981e5ebefea03dde022c8e9e253e01c7f
Metin San <metinisan@gmail.com>
2022-04-03 10:14:43 +0200

    small bug fix to integral function

--- zodipy/_integral.py
+++ zodipy/_integral.py
@@ -8,7 +8,7 @@ from zodipy._line_of_sight import LineOfSight
 
 
 def trapezoidal(
-    get_comp_step_emission: Callable[[float | NDArray[np.floating]], NDArray[np.floating]],
+    get_emission_step: Callable[[float | NDArray[np.floating]], NDArray[np.floating]],
     line_of_sight: LineOfSight,
 ) -> NDArray[np.floating]:
     """Returns the integrated Zodiacal emission for an IPDcomponent
@@ -17,7 +17,7 @@ def trapezoidal(
 
     Parameters
     ----------
-    get_comp_step_emission
+    get_emission_step
         Function that computes the Zodiacal emission at a step along the line
         of sight for an IPD component.
     line_of_sight
@@ -29,13 +29,15 @@ def trapezoidal(
         The line-of-sight integrated copmonent emission [W / Hz / m^2 / sr].
     """
 
-    r_min, r_max, n_steps = line_of_sight
+    r_min = line_of_sight.r_min
+    r_max = line_of_sight.r_max
+    n_steps = line_of_sight.n_steps
     dr = line_of_sight.dr
 
-    integrated_emission = get_comp_step_emission(r_min)
-    integrated_emission += get_comp_step_emission(r_max)
+    integrated_emission = get_emission_step(r_min)
+    integrated_emission += get_emission_step(r_max)
     integrated_emission += 2 * sum(
-        get_comp_step_emission(dr * step) for step in range(1, n_steps)
+        get_emission_step(r_min + dr * step) for step in range(1, n_steps)
     )
 
     return integrated_emission * (dr / 2)
