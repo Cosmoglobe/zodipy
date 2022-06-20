1156d31ea0070496422ffb632bc042698a3f15d7
Metin San <metinisan@gmail.com>
2022-06-20 14:45:29 +0200

    Deployed 39dbd14 with MkDocs version: 1.3.0

---
+++ examples/get_emission_ang.py
@@ -0,0 +1,26 @@
+import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.time import Time
+from zodipy import Zodipy
+
+model = Zodipy("dirbe")
+
+theta = np.linspace(0, np.pi, 10000) * u.rad
+phi = np.zeros_like(theta)
+
+emission = model.get_emission_ang(
+    30 * u.micron,
+    theta=theta,
+    phi=phi,
+    obs_time=Time("2022-06-14"),
+    obs="earth",
+)
+
+
+plt.plot(emission)
+plt.title("Zodiacal emission along theta with phi = 0")
+plt.xlabel("Theta [rad]")
+plt.ylabel("Emission [MJy/sr]")
+plt.savefig("zodiacal_emission_theta.png")
+plt.show()
