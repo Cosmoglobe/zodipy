Improved hypothesis testing. Fixed bug with masking of solar_cut

tests/_strategies.py
@@ -1,127 +1,176 @@
 from __future__ import annotations
 
 import datetime
-import random
+from functools import partial
 from math import log2
-from typing import Callable
+from typing import Any, Callable
 
 import astropy.units as u
 import healpy as hp
 import numpy as np
+from astropy.coordinates import get_body_barycentric
 from astropy.time import Time
 from hypothesis.extra import numpy
 from hypothesis.strategies import (
     DrawFn,
     SearchStrategy,
     booleans,
+    builds,
     composite,
     datetimes,
     floats,
     integers,
+    lists,
+    sampled_from,
 )
 from numpy.typing import NDArray
 
 import zodipy
 
-from ._helpers import get_random_model
-
-MIN_FREQ = u.Quantity(0.1, u.GHz)
-MAX_FREQ = u.Quantity(500, u.micron).to(u.GHz, equivalencies=u.spectral())
+MIN_FREQ = u.Quantity(10, u.GHz)
+MAX_FREQ = u.Quantity(0.1, u.micron).to(u.GHz, equivalencies=u.spectral())
+N_FREQS = 1000
+FREQ_RANGE = np.geomspace(
+    np.log(MIN_FREQ.value),
+    np.log(MAX_FREQ.value),
+    N_FREQS,
+).tolist()
 MIN_DATE = datetime.datetime(year=1900, month=1, day=1)
 MAX_DATE = datetime.datetime(year=2100, month=1, day=1)
 MIN_NSIDE = 8
 MAX_NSIDE = 1024
+MAX_PIXELS_LEN = 10000
+MAX_ANGELS_LEN = 10000
+AVAILABLE_MODELS = zodipy.model_registry.models
+OBS_TIME = Time("2020-01-01")
 
 
 @composite
-def angles(draw: DrawFn) -> tuple[u.Quantity[u.deg], u.Quantity[u.deg]]:
-    angles_shapes = numpy.array_shapes(
-        min_dims=1, max_dims=1, min_side=1, max_side=10000
-    )
-    shape = draw(angles_shapes)
-    theta = numpy.arrays(
-        dtype=float,
-        shape=shape,
-        elements=floats(min_value=0, max_value=180),
-    )
-    phi = numpy.arrays(
-        dtype=float,
-        shape=shape,
-        elements=floats(min_value=0, max_value=360),
-    )
-    return u.Quantity(draw(theta), u.deg), u.Quantity(draw(phi), u.deg)
+def quantities(
+    draw: Callable[[SearchStrategy[float]], float],
+    min_value: float,
+    max_value: float,
+    unit: u.Unit,
+) -> u.Quantity:
+    return u.Quantity(draw(floats(min_value=min_value, max_value=max_value)), unit)
 
 
 @composite
 def time(
     draw: Callable[[SearchStrategy[datetime.datetime]], datetime.datetime]
 ) -> Time:
-    datetime = draw(datetimes(min_value=MIN_DATE, max_value=MAX_DATE))
-    return Time(datetime)
+    return Time(draw(datetimes(min_value=MIN_DATE, max_value=MAX_DATE)))
 
 
 @composite
 def nside(draw: Callable[[SearchStrategy[int]], int]) -> int:
     power = draw(
-        integers(min_value=int(log2(MIN_NSIDE)), max_value=int(log2(MAX_NSIDE)))
+        integers(
+            min_value=int(log2(MIN_NSIDE)),
+            max_value=int(log2(MAX_NSIDE)),
+        )
     )
     return 2**power
 
 
 @composite
+def pixels(draw: DrawFn, nside: int) -> list[int] | NDArray[np.integer]:
+    npix = hp.nside2npix(nside)
+    pixel_strategy = integers(min_value=0, max_value=npix - 1)
+    shape = draw(integers(min_value=1, max_value=npix - 1))
+    use_array = draw(booleans())
+    if use_array:
+        return draw(numpy.arrays(dtype=int, shape=shape, elements=pixel_strategy))
+
+    return draw(lists(pixel_strategy, min_size=shape, max_size=shape))
+
+
+@composite
+def angles(
+    draw: DrawFn, lonlat: bool = False
+) -> tuple[u.Quantity[u.deg], u.Quantity[u.deg]]:
+    if lonlat:
+        theta_strategy = floats(min_value=0, max_value=360)
+        phi_strategy = floats(min_value=-90, max_value=90)
+    else:
+        theta_strategy = floats(min_value=0, max_value=180)
+        phi_strategy = floats(min_value=0, max_value=360)
+
+    shape = draw(integers(min_value=1, max_value=MAX_ANGELS_LEN))
+    use_array = draw(booleans())
+    if use_array:
+        theta = draw(numpy.arrays(dtype=float, shape=shape, elements=theta_strategy))
+        phi = draw(numpy.arrays(dtype=float, shape=shape, elements=phi_strategy))
+    else:
+        theta = draw(lists(theta_strategy, min_size=shape, max_size=shape))
+        phi = draw(lists(phi_strategy, min_size=shape, max_size=shape))
+
+    return u.Quantity(theta, u.deg), u.Quantity(phi, u.deg)
+
+
+@composite
+def freq(
+    draw: DrawFn, model: zodipy.Zodipy
+) -> u.Quantity[u.GHz] | u.Quantity[u.micron]:
+
+    if model.extrapolate:
+        return u.Quantity(np.exp(draw(sampled_from(FREQ_RANGE))), u.GHz)
+
+    min_freq = model.model.spectrum.value[0]
+    max_freq = model.model.spectrum.value[-1]
+
+    freq_range = np.geomspace(np.log(min_freq), np.log(max_freq), N_FREQS).tolist()
+    return u.Quantity(np.exp(draw(sampled_from(freq_range))), model.model.spectrum.unit)
+
+
+@composite
+def obs(draw: DrawFn, model: zodipy.Zodipy) -> str:
+    def get_obs_dist(obs: str) -> u.Quantity[u.AU]:
+        if obs == "semb-l2":
+            obs_pos = get_body_barycentric("earth", OBS_TIME).to_cartesian().xyz
+        else:
+            obs_pos = get_body_barycentric(obs, OBS_TIME).to_cartesian().xyz
+        return u.Quantity(np.linalg.norm(obs_pos.value), u.AU)
+
+    los_dist_cut = model.los_dist_cut
+    return draw(
+        sampled_from(model.supported_observers).filter(
+            lambda obs: get_obs_dist(obs) < los_dist_cut
+        )
+    )
+
+
+@composite
 def model(
     draw: DrawFn,
-    model_name: str | None = None,
-    quad_points: int | None = None,
+    model: str | None = None,
+    gauss_quad_order: int | None = None,
     extrapolate: bool | None = None,
-    los_cutoff: u.Quantity[u.AU] | None = None,
-    solar_cutoff: u.Quantity[u.deg] | None = None,
+    los_dist_cut: u.Quantity[u.AU] | None = None,
+    solar_cut: u.Quantity[u.deg] | None = None,
 ) -> zodipy.Zodipy:
-    if model_name is None:
-        model_name = get_random_model()
-    if quad_points is None:
-        quad_points = draw(integers(min_value=1, max_value=200))
-    if extrapolate is None:
-        extrapolate = draw(booleans())
-    if los_cutoff is None:
-        los_cutoff_draw = draw(floats(min_value=3, max_value=100))
-        los_cutoff = u.Quantity(los_cutoff_draw, u.AU)
-    if solar_cutoff is None:
-        solar_cutoff_draw = draw(floats(min_value=0, max_value=360))
-        solar_cutoff = u.Quantity(solar_cutoff_draw, u.deg)
 
-    return zodipy.Zodipy(
-        model_name,
-        gauss_quad_order=quad_points,
-        extrapolate=extrapolate,
-        los_cutoff=los_cutoff,
-        solar_cutoff=solar_cutoff,
-    )
-
-
-@composite
-def pixels_strategy(
-    draw: Callable[[SearchStrategy[NDArray[np.integer]]], NDArray[np.integer]],
-    nside: int,
-) -> NDArray[np.integer]:
-    pixel_shapes = numpy.array_shapes(
-        min_dims=1, max_dims=1, min_side=1, max_side=10000
-    )
-    npix = hp.nside2npix(nside) - 1
-    pixels = numpy.arrays(
-        dtype=int, shape=pixel_shapes, elements=integers(min_value=0, max_value=npix)
-    )
-    return draw(pixels)
-
-
-@composite
-def freq_strategy(
-    draw: Callable[[SearchStrategy[float]], float], model: zodipy.Zodipy
-) -> u.Quantity[u.GHz]:
-    if model.extrapolate:
-        freq = draw(floats(min_value=MIN_FREQ.value, max_value=MAX_FREQ.value))
+    strategies: dict[str, SearchStrategy[Any]] = {}
+    static_params: dict[str, Any] = {}
+    if model is None:
+        strategies["model"] = sampled_from(AVAILABLE_MODELS)
     else:
-        a, b = model.model.spectrum.value[0], model.model.spectrum.value[-1]
-        freq = random.uniform(a, b)
+        static_params["model"] = model
+    if gauss_quad_order is None:
+        strategies["gauss_quad_order"] = integers(min_value=1, max_value=200)
+    else:
+        static_params["gauss_quad_order"] = gauss_quad_order
+    if extrapolate is None:
+        strategies["extrapolate"] = booleans()
+    else:
+        static_params["extrapolate"] = extrapolate
+    if los_dist_cut is None:
+        strategies["los_dist_cut"] = quantities(min_value=3, max_value=100, unit=u.AU)
+    else:
+        static_params["los_dist_cut"] = los_dist_cut
+    if solar_cut is None:
+        strategies["solar_cut"] = quantities(min_value=0, max_value=360, unit=u.deg)
+    else:
+        static_params["solar_cut"] = solar_cut
 
-    return u.Quantity(freq, model.model.spectrum.unit)
+    return draw(builds(partial(zodipy.Zodipy, **static_params), **strategies))

tests/test_get_emission.py
@@ -1,171 +1,78 @@
-import pytest
+from __future__ import annotations
 
 import astropy.units as u
-from astropy.time import Time
 import healpy as hp
-import numpy as np
+from astropy.time import Time
+from hypothesis import given, settings
+from hypothesis.strategies import DataObject, data
 
-time = Time.now()
+import zodipy
 
-
-def test_single_pix(DIRBE):
-    emission = DIRBE.get_emission_pix(
-        34 * u.micron,
-        pixels=2342,
-        nside=64,
-        obs="earth",
-        obs_time=time,
-    )
-    assert np.size(emission) == 1
+from ._strategies import angles, freq, model, nside, obs, pixels, time
 
 
-def test_single_pointing(DIRBE):
-    emission = DIRBE.get_emission_ang(
-        34 * u.micron,
-        theta=1 * u.deg,
-        phi=30 * u.deg,
-        obs="earth",
-        obs_time=time,
-    )
-    assert np.size(emission) == 1
+@given(model(), time(), nside(), data())
+@settings(deadline=None)
+def test_get_emission_pix(
+    model: zodipy.Zodipy,
+    time: Time,
+    nside: int,
+    data: DataObject,
+) -> None:
 
-    emission = DIRBE.get_emission_ang(
-        34 * u.micron,
-        theta=170 * u.deg,
-        phi=-60 * u.deg,
-        obs="earth",
-        obs_time=time,
-        lonlat=True,
-    )
-    assert np.size(emission) == 1
+    observer = data.draw(obs(model))
+    frequency = data.draw(freq(model))
+    pix = data.draw(pixels(nside))
 
-    # test theta out of range
-    with pytest.raises(ValueError):
-        DIRBE.get_emission_ang(
-            34 * u.micron,
-            theta=200 * u.deg,
-            phi=150 * u.deg,
-            obs="earth",
-            obs_time=time,
-        )
-
-
-def test_multi_pointing(DIRBE):
-    emission = DIRBE.get_emission_ang(
-        34 * u.micron,
-        theta=[50, 80, 170] * u.deg,
-        phi=[50, 30, 20] * u.deg,
-        obs="earth",
-        obs_time=time,
-    )
-    assert np.size(emission) > 1
-
-    emission = DIRBE.get_emission_ang(
-        34 * u.micron,
-        theta=[50, 80, 180] * u.deg,
-        phi=[50, 30, 20] * u.deg,
-        obs="earth",
-        obs_time=time,
-        lonlat=True,
-    )
-    assert np.size(emission) > 1
-
-    # test theta out of range
-    with pytest.raises(ValueError):
-        DIRBE.get_emission_ang(
-            34 * u.micron,
-            theta=[50, 80, 300] * u.deg,
-            phi=[50, 30, 20] * u.deg,
-            obs="earth",
-            obs_time=time,
-        )
-
-
-def test_binned(DIRBE):
-    nside = 32
-    npix = hp.nside2npix(nside)
-    theta, phi = hp.pix2ang(nside, np.arange(npix))
-
-    # test shape == npix
-    emission = DIRBE.get_binned_emission_ang(
-        34 * u.micron,
-        theta=theta * u.deg,
-        phi=phi * u.deg,
+    emission = model.get_emission_pix(
+        frequency,
+        pixels=pix,
         nside=nside,
-        obs="earth",
         obs_time=time,
+        obs=observer,
     )
-    assert np.size(emission) == npix
+    assert len(emission) == len(pix)
 
-    emission = DIRBE.get_binned_emission_ang(
-        34 * u.micron,
-        theta=theta[:100] * u.deg,
-        phi=phi[:100] * u.deg,
+    emission_binned = model.get_binned_emission_pix(
+        frequency,
+        pixels=pix,
         nside=nside,
-        obs="earth",
         obs_time=time,
+        obs=observer,
     )
-    assert np.size(emission) == npix
+    assert emission_binned.shape == (hp.nside2npix(nside),)
 
 
-def test_time(DIRBE):
-    # test non supported time
-    with pytest.raises(AttributeError):
-        DIRBE.get_emission_pix(
-            34 * u.micron,
-            pixels=2342,
-            nside=64,
-            obs="earth",
-            obs_time="2020-10-10",
-        )
+@given(model(), time(), nside(), angles(), data())
+@settings(deadline=None)
+def test_get_emission_ang(
+    model: zodipy.Zodipy,
+    time: Time,
+    nside: int,
+    angles: tuple[u.Quantity[u.deg], u.Quantity[u.deg]],
+    data: DataObject,
+) -> None:
 
+    theta, phi = angles
 
-def test_obs_pos_vs_obs(DIRBE):
-    # test close value for manual position vs obs
-    from astropy.coordinates import get_body, HeliocentricMeanEcliptic
+    observer = data.draw(obs(model))
+    frequency = data.draw(freq(model))
 
-    earth_skycoord = get_body("Earth", time=time)
-    earth_skycoord = earth_skycoord.transform_to(HeliocentricMeanEcliptic)
-    earth_pos = earth_skycoord.represent_as("cartesian").xyz.to(u.AU)
-
-    emission_obs_pos = DIRBE.get_emission_pix(
-        34 * u.micron,
-        pixels=2342,
-        nside=64,
-        obs_pos=earth_pos,
+    emission = model.get_emission_ang(
+        frequency,
+        theta=theta,
+        phi=phi,
         obs_time=time,
+        obs=observer,
     )
+    assert emission.shape == theta.shape == phi.shape
 
-    emission_obs = DIRBE.get_emission_pix(
-        34 * u.micron,
-        pixels=2342,
-        nside=64,
-        obs="earth",
+    emission_binned = model.get_binned_emission_ang(
+        frequency,
+        theta=theta,
+        phi=phi,
+        nside=nside,
         obs_time=time,
+        obs=observer,
     )
-
-    assert np.isclose(emission_obs, emission_obs_pos)
-
-
-def test_return_comps(DIRBE):
-    no_comps = DIRBE.get_emission_pix(
-        34 * u.micron,
-        pixels=2342,
-        nside=64,
-        obs="earth",
-        obs_time=time,
-    )
-
-    comps = DIRBE.get_emission_pix(
-        34 * u.micron,
-        pixels=2342,
-        nside=64,
-        obs="earth",
-        obs_time=time,
-        return_comps=True,
-    )
-
-    assert no_comps.shape[0] == 1
-    assert comps.shape[0] == DIRBE.model.n_comps
-
-    assert np.isclose(comps.sum(axis=0), no_comps)
+    assert emission_binned.shape == (hp.nside2npix(nside),)

zodipy/_line_of_sight.py
@@ -15,7 +15,7 @@ def get_line_of_sight_endpoints(
 
     r = np.sqrt(x**2 + y**2 + z**2)
     if cutoff < r:
-        raise ValueError("cutoff must be futher away than the observer's position.")
+        raise ValueError(f"los_dist_cut is {cutoff} but observer_pos is {r}")
 
     theta = np.arctan2(y, x)
 

zodipy/_source_functions.py
@@ -86,8 +86,8 @@ def get_scattering_angle(
     -------
         Scattering angle.
     """
-
     cos_theta = (X_los * X_helio).sum(axis=0) / (R_los * R_helio)
+    cos_theta = np.clip(cos_theta, -1, 1)
 
     return np.arccos(-cos_theta)
 

zodipy/zodipy.py
@@ -52,15 +52,17 @@ class Zodipy:
             valid model range. Default is False.
         gauss_quad_order (int): Order of the Gaussian-Legendre quadrature used to evaluate
             the brightness integral. Default is 50 points.
-        los_cutoff (u.Quantity[u.AU]): Radial distance from the Sun at which all line of
+        los_dist_cut (u.Quantity[u.AU]): Radial distance from the Sun at which all line of
             sights are truncated. Defaults to 5.2 AU which is the distance to Jupiter.
-        solar_cutoff (u.Quantity[u.deg]): Cutoff angle from the sun in degrees. The emission
+        solar_cut (u.Quantity[u.deg]): Cutoff angle from the sun in degrees. The emission
             for all the pointing with angular distance between the sun smaller than
             `solar_cutoff` are set to `np.nan`. This is due to the model singularity of the
             diffuse cloud component at the heliocentric origin. Such a cutoff may be
             useful when using simulated pointing, but actual scanning strategies are
             unlikely to look directly at the sun. This feature is turned of by setting this
             argument to `None`. Defaults to 5 degrees.
+        solar_cut_fill_value (float): Fill value for the masked solar cut pointing.
+            Defaults to `np.nan`.
 
     """
 
@@ -70,17 +72,17 @@ class Zodipy:
         ephemeris: str = "de432s",
         extrapolate: bool = False,
         gauss_quad_order: int = 100,
-        los_cutoff: u.Quantity[u.AU] = DISTANCE_TO_JUPITER,
-        solar_cutoff: u.Quantity[u.deg] | None = None,
+        los_dist_cut: u.Quantity[u.AU] = DISTANCE_TO_JUPITER,
+        solar_cut: u.Quantity[u.deg] | None = None,
+        solar_cut_fill_value: float = np.nan,
     ) -> None:
 
         self.model = model_registry.get_model(model)
         self.ephemeris = ephemeris
         self.extrapolate = extrapolate
-        self.los_cutoff = los_cutoff
-        self.solar_cutoff = (
-            solar_cutoff.to(u.rad) if solar_cutoff is not None else solar_cutoff
-        )
+        self.los_dist_cut = los_dist_cut
+        self.solar_cut = solar_cut.to(u.rad) if solar_cut is not None else solar_cut
+        self.solar_cut_fill_value = solar_cut_fill_value
         self.integration_scheme = quadpy.c1.gauss_legendre(gauss_quad_order)
 
     @property
@@ -368,15 +370,6 @@ class Zodipy:
             obs=obs, obs_time=obs_time, obs_pos=obs_pos
         )
 
-        if self.solar_cutoff is not None:
-            # The observer position is aquired in geocentric coordinates before being
-            # rotated to ecliptic coordinates which means that we can find the
-            # heliocentric origin by simply taking the negative of the observer position.
-            # We set all unit_vectors with a angular distance from the suns smaller than
-            # the cutoff to np.nan which will propagate to np.nan/hp.UNSEEN in the emission.
-            ang_dist = hp.rotator.angdist(-observer_position, unit_vectors)
-            unit_vectors[:, ang_dist < self.solar_cutoff.value] = np.nan
-
         if self.model.solar_irradiance_model is not None:
             solar_irradiance = (
                 self.model.solar_irradiance_model.interpolate_solar_irradiance(
@@ -387,7 +380,7 @@ class Zodipy:
             solar_irradiance = 0
 
         start, stop = get_line_of_sight_endpoints(
-            cutoff=self.los_cutoff.value,
+            cutoff=self.los_dist_cut.value,
             obs_pos=observer_position,
             unit_vectors=unit_vectors,
         )
@@ -398,9 +391,9 @@ class Zodipy:
 
         # Preparing quantities for broadcasting
         stop_expanded = np.expand_dims(stop, axis=-1)
-        observer_position = np.expand_dims(observer_position, axis=-1)
-        earth_position = np.expand_dims(earth_position, axis=-1)
-        unit_vectors = np.expand_dims(unit_vectors, axis=-1)
+        observer_position_expanded = np.expand_dims(observer_position, axis=-1)
+        earth_position_expanded = np.expand_dims(earth_position, axis=-1)
+        unit_vectors_expanded = np.expand_dims(unit_vectors, axis=-1)
 
         # For each component, compute the integrated line of sight emission
         for idx, (label, comp) in enumerate(self.model.comps.items()):
@@ -416,9 +409,9 @@ class Zodipy:
                 _compute_comp_emission_at_step,
                 start=start,
                 stop=stop_expanded,
-                X_obs=observer_position,
-                X_earth=earth_position,
-                u_los=unit_vectors,
+                X_obs=observer_position_expanded,
+                X_earth=earth_position_expanded,
+                u_los=unit_vectors_expanded,
                 comp=comp,
                 freq=freq.value,
                 T_0=self.model.T_0,
@@ -440,13 +433,21 @@ class Zodipy:
             else:
                 emission[idx] = integrated_comp_emission[indicies]
 
-        if binned:
-            # We multiply the binned map by the number of hits.
-            emission[:, pixels] *= indicies
+        if self.solar_cut is not None:
+            # The observer position is aquired in geocentric coordinates before being
+            # rotated to ecliptic coordinates. This means that we can find the
+            # heliocentric origin in this coordinate system by simply taking the negative
+            # of the observer position. The emission corresponding to unit_vectors with a
+            # angular distance (between the pointing and the sun) smaller than the
+            # specified `solar_cutoff` value is masked with the `solar_cutoff_fill_value`.
+            ang_dist = hp.rotator.angdist(-observer_position, unit_vectors)
+            solar_mask = ang_dist < self.solar_cut.value
+            if binned and pixels is not None:
+                emission[:, pixels[solar_mask]] = self.solar_cut_fill_value
+            else:
+                emission[:, solar_mask[indicies]] = self.solar_cut_fill_value
 
         return emission
-
-    # def _remove_masked_pointing
 
     def __repr__(self) -> str:
         return (