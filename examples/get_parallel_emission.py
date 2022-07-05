2bac6a26e63f72009f9925c4238b9b874658409f
Metin San <metinisan@gmail.com>
2022-07-05 11:22:48 +0200

    Deployed a27331e with MkDocs version: 1.3.0

--- examples/get_parallel_emission.py
+++ examples/get_parallel_emission.py
@@ -15,31 +15,28 @@ obs_time = Time("2020-01-01")
 model = zodipy.Zodipy()
 model_parallel = zodipy.Zodipy(parallel=True)
 
-if __name__ == "__main__":
-    start = time.perf_counter()
-    emission = model.get_binned_emission_pix(
-        40 * u.micron,
-        pixels=pixels,
-        nside=NSIDE,
-        obs_time=obs_time,
-    )
-    print(
-        "Time spent on a single CPU:", round(time.perf_counter() - start, 2), "seconds"
-    )
-    # > Time spent on a single CPU: 143.49 seconds
+start = time.perf_counter()
+emission = model.get_binned_emission_pix(
+    40 * u.micron,
+    pixels=pixels,
+    nside=NSIDE,
+    obs_time=obs_time,
+)
+print("Time spent on a single CPU:", round(time.perf_counter() - start, 2), "seconds")
+# > Time spent on a single CPU: 91.76 seconds
 
-    start = time.perf_counter()
-    emission_parallel = model_parallel.get_binned_emission_pix(
-        40 * u.micron,
-        pixels=pixels,
-        nside=NSIDE,
-        obs_time=obs_time,
-    )
-    print(
-        f"Time spent on {multiprocessing.cpu_count()} CPUs:",
-        round(time.perf_counter() - start, 2),
-        "seconds",
-    )
-    # > Time spent on 8 CPUs: 42.52 seconds
+start = time.perf_counter()
+emission_parallel = model_parallel.get_binned_emission_pix(
+    40 * u.micron,
+    pixels=pixels,
+    nside=NSIDE,
+    obs_time=obs_time,
+)
+print(
+    f"Time spent on {multiprocessing.cpu_count()} CPUs:",
+    round(time.perf_counter() - start, 2),
+    "seconds",
+)
+# > Time spent on 8 CPUs: 26.87 seconds
 
-    assert np.allclose(emission, emission_parallel)
+assert np.allclose(emission, emission_parallel)
