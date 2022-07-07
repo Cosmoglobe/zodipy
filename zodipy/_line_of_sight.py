e52272f75f41edd52a718af4fdcdb80d6489e26e
Metin San <metinisan@gmail.com>
2022-07-07 09:30:58 +0200

    Remove repeated calculations

--- zodipy/_line_of_sight.py
+++ zodipy/_line_of_sight.py
@@ -13,21 +13,15 @@ def get_line_of_sight_endpoints(
 
     x, y, z = obs_pos.flatten()
 
-    r = np.sqrt(x**2 + y**2 + z**2)
-    if cutoff < r:
+    if cutoff < (r := np.sqrt(x**2 + y**2 + z**2)):
         raise ValueError(f"los_dist_cut is {cutoff} but observer_pos is {r}")
-
-    theta = np.arctan2(y, x)
-
-    x_0 = r * np.cos(theta)
-    y_0 = r * np.sin(theta)
 
     u_x, u_y, u_z = unit_vectors
     lon = np.arctan2(u_y, u_x)
     lat = np.arcsin(u_z)
 
     cos_lat = np.cos(lat)
-    b = 2 * (x_0 * cos_lat * np.cos(lon) + y_0 * cos_lat * np.sin(lon))
+    b = 2 * (x * cos_lat * np.cos(lon) + y * cos_lat * np.sin(lon))
     c = r**2 - cutoff**2
 
     q = -0.5 * b * (1 + np.sqrt(b**2 - 4 * c) / np.abs(b))
