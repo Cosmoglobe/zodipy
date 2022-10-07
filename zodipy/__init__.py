3c897e71fcce38e787675679ca9b3926c665308b
Metin San <metinisan@gmail.com>
2022-10-07 10:51:09 +0200

    Add support for `__version__`.

--- zodipy/__init__.py
+++ zodipy/__init__.py
@@ -1,7 +1,14 @@
+import pkg_resources
+
 from . import ipd_components, source_params
 from ._contour import tabulate_density
 from .ipd_models import model_registry
 from .zodipy import Zodipy
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    ...
 
 __all__ = (
     "Zodipy",
