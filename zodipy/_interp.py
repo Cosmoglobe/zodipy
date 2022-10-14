Fix bug where bandpass weights were not flipped after unit conversion from micron to Hz

zodipy/_interp.py
@@ -27,6 +27,10 @@ def interpolate_source_parameters(
     weights: npt.NDArray[np.float64] | None = None,
 ) -> InterpolatedSourceParameters:
 
+    if not freq.unit.is_equivalent(model.spectrum.unit):
+        freq = freq.to(model.spectrum.unit, u.spectral())
+        weights = np.flip(weights) if weights is not None else weights
+
     interpolator = partial(interp1d, x=model.spectrum, fill_value="extrapolate")
     emissivities = np.asarray(
         [
@@ -48,7 +52,6 @@ def interpolate_source_parameters(
         phase_coefficients = interpolator(y=np.asarray(model.phase_coefficients))(freq)
 
     else:
-
         phase_coefficients = np.repeat(np.zeros((3, 1)), repeats=freq.size, axis=-1)
 
     if model.solar_irradiance is not None:

zodipy/zodipy.py
@@ -420,10 +420,14 @@ class Zodipy:
         )
 
         # Convert to Hz if `freq` is in units of wavelength
-        freq = freq.to(u.Hz, u.spectral())
-        # Renormalize weights with respect to the spectrum in Hz.
-        if normalized_weights is not None:
-            normalized_weights /= np.trapz(normalized_weights, freq.value)
+        if not freq.unit.is_equivalent(u.Hz):
+            freq = freq.to(u.Hz, u.spectral())
+
+            # Flip weights if `freq` is in units of wavelength
+            if normalized_weights is not None:
+                normalized_weights = np.flip(normalized_weights) / np.trapz(
+                    normalized_weights, freq.value
+                )
 
         # Some components require additional non-static parameters to be computed, such as
         # the earth-trailing feature which needs the earth position in addition to the