6b9894fc35f6541972e6bde5be01290c79ee859e
Metin San <metinisan@gmail.com>
2024-02-01 08:46:10 +0100

    Remove deprecated `pkg_resources` api

--- zodipy/__init__.py
+++ zodipy/__init__.py
@@ -1,13 +1,7 @@
-import contextlib
-
-import pkg_resources
-
 from zodipy import comps, source_params
 from zodipy._contour import tabulate_density
 from zodipy.model_registry import model_registry
 from zodipy.zodipy import Zodipy
-
-contextlib.suppress(pkg_resources.DistributionNotFound)
 
 __all__ = (
     "Zodipy",
